5979996ebce1df518e7aa5ebdabb572a8a178caa
Kea-Angel-Ntheledi <kealebogaangelntheledi@gmail.com>
2024-05-23 10:49:17 +0200

    I defined the reducer

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ const reset = () => ({ type: RESET });
 
 const initialState = { count: 0};
 
+const tallyReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            return { count: state.count + 1 };
+            case DECREMENT:
+                return { count: state.count -1 };
+                case RESET:
+                    return { count: 0 };
+                    default:
+                        return state;
+    }
+}
