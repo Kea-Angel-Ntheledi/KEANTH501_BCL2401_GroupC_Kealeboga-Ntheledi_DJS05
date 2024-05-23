95794f13a1f5ef5f9116d011c023175772672d20
Kea-Angel-Ntheledi <kealebogaangelntheledi@gmail.com>
2024-05-23 11:21:32 +0200

    Debugged and added more comments

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-//Define action types
+// Define action types
 const INCREMENT = "INCREMENT";
 const DECREMENT = "DECREMENT";
 const RESET = "RESET";
 
-//Define action creators
+// Define action creators
 const increment = () => ({ type: INCREMENT });
 const decrement = () => ({ type: DECREMENT });
 const reset = () => ({ type: RESET });
 
-//Define the initial state
-const initialState = { count: 0};
+// Define the initial state
+const initialState = { count: 0 };
 
-//Define the reducer
+// Define the reducer
 const tallyReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case INCREMENT:
-            return { count: state.count + 1 };
-            case DECREMENT:
-                return { count: state.count -1 };
-                case RESET:
-                    return { count: 0 };
-                    default:
-                        return state;
-    }
+  switch (action.type) {
+    case INCREMENT:
+      return { count: state.count + 1 };
+    case DECREMENT:
+      return { count: state.count - 1 };
+    case RESET:
+      return { count: 0 };
+    default:
+      return state;
+  }
 };
 
-//Define the store class
+// Define the Store class
 class Store {
   constructor(reducer, initialState) {
     this.reducer = reducer;
     this.state = initialState;
-    this.lesteners = [];
+    this.listeners = [];
   }
 
   getState() {
@@ -50,18 +50,17 @@ class Store {
   }
 }
 
-//Instantiate the store
+// Instantiate the store
 const store = new Store(tallyReducer, initialState);
 
-//Subscribe to state changes to log the state
+// Subscribe to state changes to log the state
 store.subscribe(() => console.log("State:", store.getState()));
 
-//Initial state logging
+// Initial state logging
 console.log("Initial State:", store.getState());
 
-//Dispatching actions  to demonstrate state changes
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(decrement());
-store.dispatch(reset());
+// Dispatching actions to demonstrate state changes
+store.dispatch(increment()); // Should log: State: { count: 1 }
+store.dispatch(increment()); // Should log: State: { count: 2 }
+store.dispatch(decrement()); // Should log: State: { count: 1 }
+store.dispatch(reset()); // Should log: State: { count: 0 }
