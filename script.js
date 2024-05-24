ea61c30f7be198dec55cb9c253417a779f958538
Kea-Angel-Ntheledi <kealebogaangelntheledi@gmail.com>
2024-05-24 15:57:51 +0200

    debugged

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const createStore = (initialState, reducer) => {
-  let state = initialState;
-  const listeners =[];
+    let state = initialState;
+    const listeners = [];
 
-  const subscribe = (listener) => {
-    listeners.push(listener);
-    return () => {
-      const index = listeners.indexOf(listener);
-      if (index > -1) {
-        listeners.splice(index, 1);
-      }
-    }
-    }
-  }
+    const subscribe = (listener) => {
+        listeners.push(listener);
+        return () => {
+            const index = listeners.indexOf(listener);
+            if (index > -1) {
+                listeners.splice(index, 1);
+            }
+        };
+    };
 
 
 // Define the reducer
