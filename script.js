1451316b8dce518c7f82edc3c625c178d7cd79dd
Kea-Angel-Ntheledi <kealebogaangelntheledi@gmail.com>
2024-05-24 16:01:01 +0200

    Commit

--- script.js
+++ script.js
@@ -24,13 +24,13 @@ const createStore = (initialState, reducer) => {
 
 
 // Define the reducer
-const tallyReducer = (state = initialState, action) => {
+const reducer = (state = { count: 0}, action) => {
   switch (action.type) {
-    case INCREMENT:
+    case 'ADD':
       return { count: state.count + 1 };
-    case DECREMENT:
+    case 'SUBTRACT':
       return { count: state.count - 1 };
-    case RESET:
+    case 'RESET':
       return { count: 0 };
     default:
       return state;
